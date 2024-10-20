7c143b4535628aa8dab0ec9e9f6a7ee59c95b1b9
José Luiz <redg7338@gmail.com>
2024-10-20 01:47:47 -0300

    More Dtos, mappers, Post and Put

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1485758e579b053487d2ef97cd57d6d3f8fea41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2532a6369df458e7e3b2135b49e71dfabf185189")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
